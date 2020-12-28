6c41716b7809a0d20e7e7b4130bbf0d852160008
Dhruv Rajesh <makerspace98@gmail.com>
2020-12-28 11:18:45 -0500

    Create keep.py

---
+++ src/keep.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+  return """
+  <title> Denbot Server </title>
+  <h2> Server is up. (yay 😌) </h2>
+  <hr>
+  <h4> About </h4>
+  <ul>
+    <li> ❓ - This is the server running the Discord '<i>Denbot</i>' by Dhruv Rajesh. </li>
+    <li> ❌- If the server displays an error saying 'Hmm, we couldn't reach your REPL', that means the server is either <b> under maintenance or down. </b> </li>
+    <li> 🧹- This server goes under maintenance regularly, however <u> not all maintenance checks will down the server. </u> </li>
+    <li> ❗ - <b> This is a development server</b>, not made for production deployments. </b> </li>
+    <li> 🌎 - Visit the Repl <a href="https://repl.it/@drvrajesh/Denbot"> here </a> </li>
+  </ul>
+  <hr>
+  <p> This server was created using Flask and HTML to bring you the content you are seeing right now. </p>
+  """
+
+def run():
+  app.run(host='0.0.0.0', port=8080)
+
+def keep():
+  t = Thread(target=run)
+  t.start()
